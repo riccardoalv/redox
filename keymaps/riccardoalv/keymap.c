ce37ee709ec4ad854d11d1f9a33992c8fa98af8f
Ricardo Alves da Silva <ricardcpu@gmail.com>
2025-05-10 18:08:47 -0400

    first commit

---
+++ keymaps/riccardoalv/keymap.c
@@ -0,0 +1,187 @@
+#include QMK_KEYBOARD_H
+
+// Tap Dance
+enum tap_dance_ids {
+    TD_HELLO,
+};
+
+void dance_hello(tap_dance_state_t *state, void *user_data) {
+    if (state->count >= 2) {
+        SEND_STRING("Hello World!");
+        reset_tap_dance(state);
+    }
+}
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_HELLO] = ACTION_TAP_DANCE_FN(dance_hello),
+};
+
+// Custom Keycodes
+enum custom_keycodes {
+    KC_ACLICK = SAFE_RANGE,
+};
+
+// Leader Key
+void leader_start_user(void) {}
+
+void leader_end_user(void) {
+    if (leader_sequence_one_key(KC_C)) {
+        tap_code16(KC_CALC);
+    } else if (leader_sequence_one_key(KC_H)) {
+        SEND_STRING("Hello World!");
+    }
+}
+
+// Layers
+enum layer_names {
+   _QWERTY,
+   _SYMB,
+   _NAV,
+   _NAV_MOUSE,
+   _ADJUST
+};
+
+// Layer Shortcuts
+#define SYM_L       MO(_SYMB)
+#define KC_ALAS     LALT_T(KC_PAST)
+#define KC_NAGR     LT(_NAV, KC_GRV)
+#define KC_NAMI     LT(_NAV, KC_MINS)
+#define KC_NENT     LT(_NAV, KC_ENT)
+#define KC_NBSPC    LT(_NAV_MOUSE, KC_BSPC)
+#define KC_SYHO     LT(_SYMB, KC_HOME)
+#define KC_SYPD     LT(_SYMB, KC_PGDN)
+#define KC_ADPU     LT(_ADJUST, KC_PGUP)
+#define KC_ADEN     LT(_ADJUST, KC_END)
+#define MUTE_MIC    KC_MEDIA_PLAY_PAUSE
+
+// Debug log on startup
+void keyboard_post_init_user(void) {
+    debug_enable = true;
+    debug_matrix = true;
+    debug_keyboard = true;
+}
+
+// Scroll Lock handling
+#define SCROLL_LOCK_MASK (1 << 2)
+
+bool is_scroll_lock_active(void) {
+    return host_keyboard_leds() & SCROLL_LOCK_MASK;
+}
+
+bool symb_layer_active = false;
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    bool is_symb = get_highest_layer(state) == _SYMB;
+
+    if (is_symb != symb_layer_active) {
+        tap_code(KC_SCROLL_LOCK);
+        symb_layer_active = is_symb;
+    }
+
+    return state;
+}
+
+// Autoclicker logic
+bool autoclick_active = false;
+uint16_t autoclick_timer = 0;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_ACLICK:
+            if (record->event.pressed) {
+                autoclick_active = !autoclick_active;
+                autoclick_timer = timer_read();
+            }
+            return false;
+    }
+    return true;
+}
+
+void matrix_scan_user(void) {
+    if (is_scroll_lock_active() != symb_layer_active) {
+        if (is_scroll_lock_active()) {
+            layer_on(_SYMB);
+        } else {
+            layer_off(_SYMB);
+        }
+    }
+
+    if (autoclick_active && timer_elapsed(autoclick_timer) > 25) {
+        tap_code(KC_MS_BTN1);
+        autoclick_timer = timer_read();
+    }
+}
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_QWERTY] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     KC_GRV  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                                            KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_MINS ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,SYM_L   ,                          SYM_L   ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_EQL  ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_ESC  ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_TAB ,                        TG(_SYMB) ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_ADEN ,KC_SYHO,         KC_SYPD ,KC_ADPU ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSFT ,
+  //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
+    KC_LGUI  ,QK_LEAD ,MO(_SYMB),KC_LALT,     KC_LSFT ,    KC_SPC  , KC_RCTL,        KC_NENT ,KC_NBSPC,    KC_RSFT ,    KC_RALT,MO(_SYMB),QK_LEAD  ,KC_H
+  //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
+  ),
+
+  [_SYMB] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     _______ ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,                                            KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  , KC_F11 ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______ ,KC_BSLS ,KC_AT   ,KC_LCBR ,KC_RCBR ,KC_PIPE ,_______ ,                          _______ ,KC_PSLS ,KC_P7   ,KC_P8   ,KC_P9   ,KC_PMNS , KC_F12 ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______ ,KC_HASH ,KC_DLR  ,KC_LBRC ,KC_RBRC ,_______  ,_______ ,                          _______ ,KC_PAST ,KC_P4   ,KC_P5   ,KC_P6   ,KC_PPLS , KC_NUM ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______ ,KC_PERC ,KC_CIRC ,KC_LPRN ,KC_RPRN ,KC_TILD ,_______ ,_______ ,        _______ ,_______ ,XXXXXXX ,KC_P1   ,KC_P2   ,KC_P3   ,KC_PENT ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
+     _______ ,_______ ,_______ ,_______ ,     _______ ,    _______ ,_______ ,        _______ ,_______ ,    KC_P0   ,     KC_P0   ,KC_PDOT ,KC_PENT ,XXXXXXX
+  //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
+  ),
+
+  [_NAV] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,                                            _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,KC_MS_U ,XXXXXXX ,KC_WH_U ,XXXXXXX ,_______ ,                          _______ ,XXXXXXX,KC_MS_BTN1,KC_MS_BTN2,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,KC_MS_L ,KC_MS_D ,KC_MS_R ,KC_WH_D ,XXXXXXX ,_______ ,                          _______ ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RIGHT,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,_______ ,        _______ ,XXXXXXX ,KC_DEL , XXXXXXX, XXXXXXX,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,     KC_BTN1 ,    KC_BTN2 ,_______ ,        _______ ,_______ ,    XXXXXXX ,     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX
+  //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
+  ),
+
+  [_NAV_MOUSE] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,                                           _______ , KC_ACLICK ,_______ ,_______ ,_______ ,_______ ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,                          _______,XXXXXXX,KC_MS_BTN1,KC_MS_BTN2,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,                          _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R ,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,        _______ ,XXXXXXX ,KC_WH_L ,KC_WH_D ,KC_WH_U ,KC_WH_R ,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
+     _______ ,_______ ,_______ ,_______ ,     _______ ,    _______ ,_______ ,        _______ ,_______ ,    XXXXXXX ,     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX
+  //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
+  ),
+
+
+  [_ADJUST] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     XXXXXXX, KC_BRID, KC_BRIU, XXXXXXX, XXXXXXX, KC_INT1 ,                                            XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,KC_CALC ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,KC_F13  ,KC_F14  ,KC_F15  ,KC_F16  ,KC_F17  ,KC_VOLD,                           KC_VOLU ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,KC_PSCR ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,KC_F18  ,KC_F19  ,KC_F20  ,KC_F21  ,KC_F22  ,KC_MUTE ,                          XXXXXXX ,KC_MPRV ,XXXXXXX ,KC_MPLY ,KC_MNXT ,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,KC_F23  ,KC_F24  ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,XXXXXXX ,        XXXXXXX ,_______ ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,     QK_BOOT ,    XXXXXXX ,XXXXXXX ,        XXXXXXX ,XXXXXXX ,    QK_BOOT ,     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX
+  //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
+  )
+};
